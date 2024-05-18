4da5ad3b43649309ba3aecbc196788a5805f89c1
KandipatiVenkataSaroojini <155419027+KandipatiVenkataSaroojini@users.noreply.github.com>
2024-05-18 14:07:30 +0530

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,9 @@
+a =60
+b=a-70
+c=x=y=0
+z=console.log("program worked!")
+console.log(z)
+age=10
+age>18 && console.log("you can vote!!!")           //operators short circuit
+age>18 || console.log("you can'td  vote!!!")   
+age>18 ? console.log("you can vote"):console.log("you can't vote")
